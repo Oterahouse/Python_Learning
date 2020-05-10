14054ddd116fc41fdc71d2061dd62046a4030d4f
Otera <tk.0624.35@gmail.com>
2020-05-11 00:15:57 +0900

    20200511_9章チャレンジ3

--- Python_Learning.py
+++ Python_Learning.py
@@ -1,6 +1,10 @@
-my_list = []
+import csv
 
-with open("st.txt", "r") as f:
-    my_list.append(f.read())
+lists = [["Top Gun", "Risky Business", "Minority Report"],
+         ["Titanic", "The Revenant", "Inception"],
+         ["Training Day", "Mon on File", "Flight"]]
 
-print(my_list)
+with open("st.csv", "w", newline="") as f:
+    w = csv.writer(f,delimiter=",")
+    for list in lists:
+        w.writerow(list)
