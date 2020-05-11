ccf022f18023f7a9c8420f97ab0ef36217d412ad
Otera <tk.0624.35@gmail.com>
2020-05-11 23:49:54 +0900

    ハングマン

---
+++ hangman.py
@@ -0,0 +1,13 @@
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "________      ",
+              "|      |      ",
+              "|      0      ",
+              "|     /|\     ",
+              "|     / \     ",
+              "|"]
+    rletters = list(word)
+    board  ["_"] * len((word)
+    win = False
+    print("ハングマンへようこそ!")
