16556ae6502441b944b6e3cf0cdbc63a0183bbb3
Otera <tk.0624.35@gmail.com>
2020-05-13 02:28:15 +0900

    20200513_オブジェクト指向

--- Python_Learning.py
+++ Python_Learning.py
@@ -1,26 +1,9 @@
-pop = []
-jpop = []
+class Orange:
+    def __init__(self, w, c):
+        self.weight = w
+        self.color = c
+        print("Created!")
 
-def collect_songs():
-    song = "曲名を入力してください:"
-    ask = "pかjのどちらかを入力してください。qで終わります。"
-
-    while True:
-        genre = input(ask)
-
-        if genre == "q":
-            break
-
-        if genre == "p":
-            pop.append(input(song))
-
-        elif genre == "j":
-            jpop.append(input(song))
-
-        else:
-            print("不正な値です")
-
-    print("pop songs:", pop)
-    print("jpop songs:", jpop)
-
-collect_songs()
+orl = Orange(10, "dark orange")
+print(orl.weight)
+print(orl.color)
