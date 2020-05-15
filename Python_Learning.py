0d39597738c998e6145a62cfb16fae92b89bef7a
Otera <tk.0624.35@gmail.com>
2020-05-16 02:08:02 +0900

    13章チャレンジ３

--- Python_Learning.py
+++ Python_Learning.py
@@ -1,4 +1,12 @@
-class Square:
+class Shape:
+    def __init__(self):
+        pass
+
+    def what_am_i(self):
+        print("I am a shape")
+
+
+class Square(Shape):
     def __init__(self, a1, a2, a3, a4):
         self.a1 = a1
         self.a2 = a2
@@ -14,13 +22,14 @@ class Square:
         self.a3 = self.a3 + change
         self.a4 = self.a4 + change
 
-class Rectangle:
+class Rectangle(Shape):
     def __init__(self, b1, b2):
         self.b1 = b1
         self.b2 = b2
 
     def calculate_perimeter(self):
         return self.b1 * 2 + self.b2 * 2
+
 
 r = Rectangle(1, 2)
 print(r.calculate_perimeter())
@@ -30,3 +39,6 @@ s.change_size(3)
 print(s.calculate_perimeter())
 s.change_size(-2)
 print(s.calculate_perimeter())
+
+r.what_am_i()
+s.what_am_i()
