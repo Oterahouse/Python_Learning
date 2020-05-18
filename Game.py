6a7c37024cd3dd713b761881405a02a896cbc6ab
Otera <tk.0624.35@gmail.com>
2020-05-18 23:41:40 +0900

    15章

--- Game.py
+++ Game.py
@@ -1,64 +1,6 @@
-from random import shuffle
-
-class Card:
-    suits = ["spades", "hearts", "diamonds", "clubs"]
-
-    values = [None, None,
-              "2", "3", "4", "5", "6", "7", "8", "9",
-              "10", "Jack", "Queen", "King", "Ace"]
-
-    def __init__(self, v, s):
-        self.value = v
-        self.suit = s
-
-    def __lt__(self, c2):
-        if self.value < c2.value:
-            return True
-
-        if self.value == c2.value:
-            if self.suit < c2.suit:
-                return True
-            else:
-                return False
-
-        return False
-
-    def __gt__(self, c2):
-        if self.value > c2.value:
-            return True
-
-        if self.value == c2.value:
-            if self.suit > c2.suit:
-                return True
-
-            else:
-                return False
-        return False
-
-    def __repr__(self):
-        v = self.values[self.value] + " of " \
-            + self.suits[self.suit]
-
-        return v
-
-class Deck:
-    def __init__(self):
-        self.cards = []
-        for i in range(2,15):
-            for j in range(4):
-                self.cards.append(Card(i,j))
-        shuffle(self.cards)
-
-    def rm_card(self):
-        if len(self.cards) == 0:
-            return
-        return self.cards.pop()
-
-class Player:
-    def __init__(self, name):
-        self.wins = 0
-        self.card = None
-        self.name = name
+import Card
+import Deck
+import Player
 
 class Game:
     def __init__(self):
@@ -77,7 +19,7 @@ class Game:
         print(d)
 
     def play_game(self):
-        cards = self.deck.cards
+        card = self.deck.cards
         print("戦争を始めます！")
         while len(cards) >= 2:
             m = "q で終了、それ以外のキーでPlay:"
