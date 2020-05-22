86e0baed04b4b213f27b81c741be303dfce7178a
Otera <tk.0624.35@gmail.com>
2020-05-22 21:54:32 +0900

    Linear_Search

---
+++ Linear_Search.py
@@ -0,0 +1,23 @@
+import random
+
+def ss(number_list, n):
+    found = False
+    for i in number_list:
+        if i == n:
+            found = True
+            break
+
+    return found
+
+# numbers = range(0, 100)
+numbers = []
+for number in range(0,101):
+    ran = random.randint(0,100)
+    numbers.append(ran)
+
+print(str(numbers) + "\n")
+s1 = ss(numbers, 2)
+print(s1)
+
+s2 = ss(numbers, 202)
+print(s2)
