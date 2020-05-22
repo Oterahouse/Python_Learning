a33d7f67f7c7236705321b28dc0df35c17e94e9c
Otera <tk.0624.35@gmail.com>
2020-05-22 23:38:53 +0900

    Algorithm

--- Algorithm.py
+++ Algorithm.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from collections import defaultdict
+
 def count_characters(string):
     count_dict= {}
     for c in string:
@@ -8,4 +11,15 @@ def count_characters(string):
 
     print(count_dict)
 
+def count_defaultdict(string):
+    count_defaultdict = defaultdict(int)
+    for C in string:
+        count_defaultdict[C] += 1
+
+    print(count_defaultdict)
+
 count_characters("Dynasty")
+
+count_defaultdict("Dynasty")
+
+print(Counter("Dynasty"))
