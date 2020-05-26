71ba4c95873b322302f642015d5c71f99430238d
Otera <tk.0624.35@gmail.com>
2020-05-26 22:12:56 +0900

    My Django Girsl app, first commit

---
+++ Django_Girls/djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tk\pycharmprojects\python_learning\python_learning\django_girls\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
