de43b63aeb6cc1fae836b53e7f12d4649fcce6ab
Otera <tk.0624.35@gmail.com>
2020-05-28 23:48:42 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- Django_Girls/djangogirls/blog/views.py
+++ Django_Girls/djangogirls/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from django.contrib.auth.models import User
@@ -8,3 +8,7 @@ def post_list(request):
   me = User.objects.get(username='ola')
   posts = Post.objects.filter(author=me).order_by('created_date')
   return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, 'blog/post_detail.html', {'post': post})
